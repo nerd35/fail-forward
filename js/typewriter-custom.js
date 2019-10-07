a0fee4d9c345d0e0fd46bacaf1bf97237e0ebf75
Umukoro <umukorochinedu@gmail.com>
2019-10-07 01:33:48 +0100

    fail forward website

---
+++ js/typewriter-custom.js
@@ -0,0 +1,8 @@
+/* ========= Typewriter effect =========== */
+/* Ref: https://safi.me.uk/typewriterjs/ */
+const instance = new Typewriter('#typewriter', {
+    strings: ['BE SEEN', 'BE HEARD', 'GO GLOBAL '],
+    autoStart: true,
+    loop: true,
+  });
+
