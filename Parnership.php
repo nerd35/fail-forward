69cd4567b189fed0367294dd696b17f1126dca47
Umukoro <umukorochinedu@gmail.com>
2019-10-09 14:17:59 +0100

    form authentication

---
+++ Parnership.php
@@ -0,0 +1,43 @@
+<?php
+if(isset( $_POST['norg']))
+$norg = $_POST['norg'];
+if(isset( $_POST['fname']))
+$fname = $_POST['fname'];
+if(isset( $_POST['lname']))
+$lname = $_POST['lname'];
+if(isset( $_POST['pnumber']))
+$pnumber = $_POST['pnumber'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['cwebsite']))
+$cwebsite = $_POST['cwebsite'];
+if(isset( $_POST['country']))
+$country = $_POST['country'];
+if(isset( $_POST['city']))
+$city = $_POST['city'];
+if(isset( $_POST['companysm']))
+$companysm = $_POST['companysm'];
+if(isset( $_POST['topart']))
+$topart = $_POST['topart'];
+if(isset( $_POST['fbook']))
+$fbook = $_POST['fbook'];
+if(isset( $_POST['linkedin']))
+$linkedin = $_POST['linkedin'];
+if(isset( $_POST['instagram']))
+$instagram = $_POST['instagram'];
+if(isset( $_POST['twitter']))
+$twitter = $_POST['twitter'];
+if(isset( $_POST['others']))
+$others = $_POST['others'];
+
+
+$content="First Name: $fname \n Last Name: $lname \n Name Of organisation: $norg \n Phone Number: $pnumber \n Email: $email \n Company Website: $cwebsite \n Country: $country \n City: $city \n Company Social Media: $companysm \n Type of Partnership: $topart \n Facebook: $fbook \n LinkedIn: $linkedin \n Instagram: $instagram \n Twitter: $twitter \n Others: $others ";
+$recipient = "queenesohe@gmail.com";
+$subject = "Partnership Application";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+
+/* Redirect visitor to the thank you page */
+header('Location: thanku.html');
+exit();
+?>
---
+++ membership.php
@@ -0,0 +1,47 @@
+<?php
+if(isset( $_POST['fname']))
+$fname = $_POST['fname'];
+if(isset( $_POST['lname']))
+$lname = $_POST['lname'];
+if(isset( $_POST['startup']))
+$startup = $_POST['startup'];
+if(isset( $_POST['pnumber']))
+$pnumber = $_POST['pnumber'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['title']))
+$title = $_POST['title'];
+if(isset( $_POST['country']))
+$country = $_POST['country'];
+if(isset( $_POST['region']))
+$region = $_POST['region'];
+if(isset( $_POST['citychap']))
+$citychap = $_POST['citychap'];
+if(isset( $_POST['campuschap']))
+$campuschap = $_POST['campuschap'];
+if(isset( $_POST['fbook']))
+$fbook = $_POST['fbook'];
+if(isset( $_POST['linkedin']))
+$linkedin = $_POST['linkedin'];
+if(isset( $_POST['instagram']))
+$instagram = $_POST['instagram'];
+if(isset( $_POST['twitter']))
+$twitter = $_POST['twitter'];
+if(isset( $_POST['howdid']))
+$howdid = $_POST['howdid'];
+if(isset( $_POST['others']))
+$others = $_POST['others'];
+if(isset( $_POST['message']))
+$message = $_POST['message'];
+
+
+$content="First Name: $fname \n Last Name: $lname \n Name of startup: $startup \n Phone Number: $pnumber \n Email: $email \n Title: $title \n Country: $country \n Region: $region \n City Chapter: $citychap \n Campus Chapter: $campuschap \n Facebook: $fbook \n LinkedIn: $linkedin \n Instagram: $instagram \n Twitter: $twitter \n How did you Here About us: $howdid \n Others: $others \n Message: $message";
+$recipient = "queenesohe@gmail.com";
+$subject = "Join Membership Application";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+
+/* Redirect visitor to the thank you page */
+header('Location: thanku.html');
+exit();
+?>
