9d9487e96c0739d96e025e2002b368a8cadb844b
Umukoro <umukorochinedu@gmail.com>
2019-10-09 22:58:32 +0100

    finishing

--- chapter.php
+++ chapter.php
@@ -35,9 +35,9 @@ if(isset( $_POST['message']))
 $message = $_POST['message'];
 
 
-$content="First Name: $fname \n Last Name: $lname \n Name of startup: $startup \n Phone Number: $pnumber \n Email: $email \n Title: $title \n Country: $country \n Region: $region \n Host City: $citychap \n Host Campus : $campuschap \n Facebook: $fbook \n LinkedIn: $linkedin \n Instagram: $instagram \n Twitter: $twitter \n How did you Here About us: $howdid \n Others: $others \n Message: $message";
+$content="First Name: $fname \n Last Name: $lname \n Name of startup: $startup \n Phone Number: $pnumber \n Email: $email \n Title: $title \n Country: $country \n Region: $region \n City Chapter: $citychap \n Campus Chapter: $campuschap \n Facebook: $fbook \n LinkedIn: $linkedin \n Instagram: $instagram \n Twitter: $twitter \n How did you Here About us: $howdid \n Others: $others \n Message: $message";
 $recipient = "queenesohe@gmail.com";
-$subject = "Request to become a host";
+$subject = "Request to join a Chapter";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 
---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+if(isset( $_POST['fname']))
+$fname = $_POST['fname'];
+if(isset( $_POST['lname']))
+$lname = $_POST['lname'];
+if(isset( $_POST['pnumber']))
+$pnumber = $_POST['pnumber'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['title']))
+$title = $_POST['title'];
+if(isset( $_POST['message']))
+$message = $_POST['message'];
+
+
+$content="First Name: $fname \n Last Name: $lname \n Title: $title \n Phone Number: $pnumber \n Email: $email \n Title: $title \n Message: $message";
+$recipient = "queenesohe@gmail.com";
+$subject = "Equiry from visitor";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+
+/* Redirect visitor to the thank you page */
+header('Location: thanku.html');
+exit();
+?>
